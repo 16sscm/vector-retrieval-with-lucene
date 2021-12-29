101666b04532ac84dbf8c4900641a377ad704de6
BaiXuefeng <xuefengbai@hiretual.com>
2021-12-29 15:00:33 +0800

    remove lfs files

--- build.sh
+++ build.sh
@@ -5,8 +5,23 @@ set -x
 G_BASE_PATH=$(cd `dirname $0`; pwd)
 cd ${G_BASE_PATH}
 
+if [ -f release/FilterKnn.tar.gz ]; then
+    \rm -rf release/FilterKnn.tar.gz
+fi
+aws s3 cp s3://htm-test/BaiXuefeng/FilterKnn/2021-12-23/FilterKnn.tar.gz release/
+
 \rm -rf ~/.gradle
-unzip -u tools/gradle-*-bin.zip
+
+if [ -f gradle-*-bin.zip ]; then
+    \rm -rf gradle-*-bin.zip
+fi
+aws s3 cp s3://htm-test/BaiXuefeng/FilterKnn/gradle-7.3.3-bin.zip .
+unzip -u gradle-*-bin.zip
+
+if [ -f release/resume-search-*.jar ]; then
+    \rm -rf release/resume-search-*.jar
+fi
 ./gradle-*/bin/gradle clean bootJar --no-daemon
 \cp -rf ./build/libs/resume-search-*.jar ./release
+
 \rm -rf ~/.gradle
